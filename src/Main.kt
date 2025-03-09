bad639ddc81335beb67b04110ada3c9ec4509bf7
kimlog126 <jisu11260@gmail.com>
2025-03-10 00:41:05 +0900

    kotlin 연습문제 1

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+fun main(){
+    val child =5
+    val adult = 28
+    val senior = 87
+
+    println("$child 살 가격: \$${ticketPrice(child)}")
+    println("$adult 살 가격: \$${ticketPrice(adult)}")
+    println("$adult 살 월요일 할인 가격: \$${ticketPrice(adult, true)}")
+    println("$senior 살 가격: \$${ticketPrice(senior)}")
+    println("1000 살 가격: \$${ticketPrice(1000)}")
+}
+
+fun ticketPrice(age:Int, isMonday: Boolean = false):Int {
+    return  when(age){
+        in 0..2 -> 15
+        in 3..60 -> if (!isMonday) 30 else 25
+        in 61..100 -> 20
+        else -> -1
+    }
+
+}
