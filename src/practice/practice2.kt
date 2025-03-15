a805fdb2417d259047352562e394fd79f9d6bc14
kimlog126 <jisu11260@gmail.com>
2025-03-16 01:14:36 +0900

    연습문제 2번 수정

--- src/practice/practice2.kt
+++ src/practice/practice2.kt
@@ -6,14 +6,13 @@ class BidderList{
     val bidders = ArrayList<Bidder>()
 
     fun add(bidder: Bidder?) {
-        // null이 아닌 경우에만 추가
         bidder?.let{bidders.add(it)}
     }
 
     fun getWinner(): Bidder? {
         return bidders
             .filter { it.money != null && it.name != null }
-            .minByOrNull { it.money!! } //filter로 걸렀지만 kotlin이 money:Int? 라고 인식 따라서 이 값은 절대 null이 아니다 !!로 명시
+            .minByOrNull { it.money!! }
             ?.copy()
     }
 }
